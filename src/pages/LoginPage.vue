53aad74c86a95b2a0af0ad84a9a3278a2e6856ef
klips-4 <95581718+klips-4@users.noreply.github.com>
2025-03-27 11:08:25 +0500

    27.03.25

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,43 +1,59 @@
 <template>
-<div class="modal-dialog modal-lg">
-  <div class="modal-content">
-    <div class="modal-body border-radius p-5 bg-light">
-      <ul class="nav nav-pills nav-justified mb-3">
-        <li class="nav-item">
-          <router-link  class="nav-link text-dark active-position" to="/login">Авторизация</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link  class="nav-link text-dark" to="/signup">Регистрация</router-link>
-        </li>
-      </ul>
-      <div class="tab-content">
-        <div class=" ">
-          <form class="text-center" action="">
-            <p>Войти с помощью:</p>
-            <div class="d-flex flex-row justify-content-center">
-              <GoogleLogin @success="onGoogleLoginSuccess" @error="onGoogleLoginError">
+  <div class="modal-dialog modal-dialog-centered" style="max-width: 400px; width: 90%">
+    <div class="modal-content">
+      <div class="modal-body border-radius p-5 bg-cst">
+
+        <ul style="border-radius: 5px" class="nav nav-pills nav-justified mb-3 bg-light">
+          <li class="nav-item">
+            <router-link  class="nav-link text-dark active-position sans-serif" to="/login">Авторизация</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link  class="nav-link text-dark sans-serif" to="/signup">Регистрация</router-link>
+          </li>
+        </ul>
+
+        <div class="tab-content">
+          <div class=" ">
+            <form class="text-center" action="">
+              <p class="text-decor">Войти с помощью:</p>
+              <div class="d-flex flex-row justify-content-center">
+                <GoogleLogin @success="onGoogleLoginSuccess" @error="onGoogleLoginError">
+                  <div class="form-outline mx-2 mb-2 cursor-pointer text-blue">
+                    <i class="bi bi-google fs-4"></i>
+                  </div>
+                </GoogleLogin>
                 <div class="form-outline mx-2 mb-2 cursor-pointer text-blue">
-                  <i class="bi bi-google fs-4"></i>
+                  <i class="bi bi-facebook fs-4"></i>
                 </div>
-              </GoogleLogin>
-            <div class="form-outline mx-2 mb-2 cursor-pointer text-blue">
-              <i class="bi bi-facebook fs-4"></i>
-            </div>
-            </div>
-            <p>или:</p>
-              <div class="form-outline mb-4">
-                <input type="text" class="form-control" v-model="username" placeholder="Email">
+              </div>
+              <div class="text-decor-container mb-3">
+                <span class="line"></span>
+                <p class="text-decor">или</p>
+                <span class="line"></span>
               </div>
               <div class="form-outline mb-4">
-                <input type="text" class="form-control" v-model="password" placeholder="Пароль">
+                <input
+                    type="text"
+                    class="form-control sans-serif"
+                    v-model="username"
+                    placeholder="Email">
               </div>
-              <button @click="authStore.login(username,password)" type="submit" class="btn btn-primary btn-block mb-1 w-100">Войти</button>
-          </form>
+              <div class="form-outline mb-4">
+                <input
+                    type="text"
+                    class="form-control sans-serif"
+                    v-model="password"
+                    placeholder="Пароль">
+              </div>
+              <button @click="authStore.login(username,password)"
+                      type="submit"
+                      class="btn btn-primary btn-block mb-1 w-100 sans-serif fw-bold">Войти</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script setup>
@@ -53,5 +69,23 @@ const password = ref('');
 
 <style lang="scss">
 
+.bg-cst {
+  background: rgba(255, 255, 255, 0.85);
+}
 
+.text-decor-container {
+  display: flex;
+  align-items: center;
+}
+
+.line {
+  flex: 1;
+  height: 1px;
+  background-color: #707070;
+  margin: 0 10px;
+}
+
+.text-decor {
+  margin: 0;
+}
 </style>
--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,35 +1,32 @@
 <template>
-  <div class="modal-dialog modal-lg">
+  <div class="modal-dialog modal-dialog-centered" style="max-width: 400px; width: 90%">
     <div class="modal-content">
       <div class="modal-body border-radius p-5 bg-light">
-        <ul class="nav nav-pills nav-justified mb-3">
-          <li class="nav-item">
-            <router-link  class="nav-link text-dark" to="/login">Авторизация</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link  class="nav-link text-dark active-position" to="/signup">Регистрация</router-link>
-          </li>
-        </ul>
+        <div class="text-center">
+          <img class="img-fluid w-25" src="../../public/logo.png" alt="">
+        </div>
+
         <div class="tab-content">
           <div class=" ">
-            <form class="text-center" action="">
-              <div class="form-outline mb-4">
-                <input type="text" class="form-control" placeholder="Фамилия">
+            <form class="text-center pt-3" action="">
+              <h5 style="color: rgba(28, 100, 255)">Зарегестрироваться в WASH`N WRENCH</h5>
+              <div class="form-outline mt-3 mb-4">
+                <input type="text" class="form-control btn-cst cursor-pointer" v-model="surname" placeholder="Фамилия">
               </div>
               <div class="form-outline mb-4">
-                <input type="text" class="form-control" placeholder="Имя">
+                <input type="text" class="form-control btn-cst cursor-pointer" v-model="firstname" placeholder="Имя">
               </div>
               <div class="form-outline mb-4">
-                <input type="text" class="form-control" placeholder="Отчество">
+                <input type="text" class="form-control btn-cst cursor-pointer" v-model="patronymic" placeholder="Отчество">
               </div>
               <div class="form-outline mb-4">
-                <input type="text" class="form-control" placeholder="Email">
+                <input type="text" class="form-control btn-cst cursor-pointer" v-model="email" placeholder="Email">
               </div>
               <div class="form-outline mb-4">
-                <input type="text" class="form-control" placeholder="Пароль">
+                <input type="text" class="form-control btn-cst cursor-pointer" v-model="password" placeholder="Email">
               </div>
-              <button type="submit" class="btn btn-primary btn-block mb-1 w-100">Зарегестрироваться</button>
-
+              <button @click="authStore.login(username,password)" type="submit" class="btn btn-cst btn-block mb-4 w-100">Подтвердить</button>
+              <router-link  class="btn btn-cst btn-block mb-1 w-100" to="/login">Назад</router-link>
             </form>
           </div>
         </div>
@@ -39,7 +36,13 @@
 </template>
 
 <script setup>
+import {ref} from "vue";
 
+const surname = ref("")
+const firstname = ref("")
+const patronymic = ref("")
+const email = ref("")
+const password = ref("")
 </script>
 
 <style lang="scss">
